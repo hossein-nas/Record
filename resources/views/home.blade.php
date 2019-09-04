e51520f016ba2b6e47b20e0f26fa75a7f109c29f
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2019-09-04 18:46:21 +0430

    some graphic changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,48 @@
 
     </div>
     <div class="cabinets">
-        <div class="row"></div>
+        <div class="row">
+            <section class="cabinet">
+                <label for="number">1</label>
+
+            </section>
+            <section class="cabinet">
+                <label for="number">2</label>
+
+            </section>
+            <section class="cabinet">
+                <label for="number">3</label>
+
+            </section>
+            <section class="cabinet">
+                <label for="number">4</label>
+
+            </section>
+            <section class="cabinet">
+                <label for="number">5</label>
+
+            </section>
+            <section class="cabinet">
+                <label for="number">6</label>
+
+            </section>
+            <section class="cabinet">
+                <label for="number">7</label>
+
+            </section>
+            <section class="cabinet">
+                <label for="number">8</label>
+
+            </section>
+            <section class="cabinet">
+                <label for="number">9</label>
+
+            </section>
+            <section class="cabinet">
+                <label for="number">10</label>
+
+            </section>
+        </div>
         <div class="row"></div>
 
     </div>
