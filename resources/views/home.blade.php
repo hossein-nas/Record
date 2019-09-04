27d44b6acb38f21df4ce221c822022b01da7d9ba
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2019-09-04 18:14:46 +0430

    some minor changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,12 @@
 @section('content')
 <div class="container">
     <div class="main-section">
+        سلام به همگی
 
     </div>
     <div class="cabinets">
+        <div class="row"></div>
+        <div class="row"></div>
 
     </div>
 </div>
